79f3b32907782e6d728a7116125a75b2e56bf0ca
Gabriel Silingardi <silingardigabriel@gmail.com>
2026-05-05 11:38:12 -0300

    Desafio Completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("REALIZANDO TESTE COM CLASSE NOKIA");
+Nokia nokia = new Nokia("12345678900", "NO26", "1123131", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Minecraft");
+
+Console.WriteLine("===---===---===---===---===---===---===---===---===---===---===---===");
+
+Console.WriteLine("REALIZANDO TESTE COM CLASSE IPHONE");
+Iphone iphone = new Iphone("00987654321", "13", "68763", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
 // TODO: Realizar os testes com as classes Nokia e Iphone
